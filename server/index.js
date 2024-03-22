7e8a4d25b35ed479c66cb4d95e11d91a0b25822a
GhasseneBenSlimene <123106762+GhasseneBenSlimene@users.noreply.github.com>
2024-03-22 15:52:30 +0100

    Change port to 8000

--- server/index.js
+++ server/index.js
@@ -41,7 +41,7 @@ app.use((error, req, res, next) => {
   res.status(500).send("Something broke!");
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, "0.0.0.0", () =>
   console.log(`Server is running on port ${PORT}`)
 );
