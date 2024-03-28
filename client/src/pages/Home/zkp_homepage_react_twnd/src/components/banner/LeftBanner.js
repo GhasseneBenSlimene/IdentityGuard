67b5e7dd0a0369c95e0c1801401c9d76aff2d5ad
Duke13019 <113926537+Duke13019@users.noreply.github.com>
2024-03-28 14:51:33 +0100

    homepage code adding

---
+++ client/src/pages/Home/zkp_homepage_react_twnd/src/components/banner/LeftBanner.js
@@ -0,0 +1,74 @@
+import React from 'react'
+import { useTypewriter, Cursor } from "react-simple-typewriter";
+import { FaFacebookF, FaTwitter, FaLinkedinIn, FaReact } from "react-icons/fa";
+import { SiTailwindcss, SiFigma, SiNextdotjs } from "react-icons/si";
+
+const LeftBanner = () => {
+    const [text] = useTypewriter({
+      words: ["Protégez vos données.", "Prouvez votre âge sans la divulguer.", "Votez en  gardant votre anonymat."],
+      loop: true,
+      typeSpeed: 20,
+      deleteSpeed: 10,
+      delaySpeed: 2000,
+    });
+  return (
+    <div className="w-full lgl:w-1/2 flex flex-col gap-20">
+      <div className="flex flex-col gap-5">
+        <h4 className=" text-lg font-normal">IdentityGuard</h4>
+        <h1 className="text-6xl font-bold text-black">
+          Avec la technologie <span className="text-designColor capitalize">ZKP</span>
+        </h1>
+        <h2 className="text-4xl font-bold text-black">
+           <span>{text}</span>
+          <Cursor
+            cursorBlinking="false"
+            cursorStyle="|"
+            cursorColor="#123456"
+          />
+        </h2>
+        <p className="text-base font-bodyFont leading-6 tracking-wide">
+          Cette animation a été faite par Ilyas. Zone de texte a exploiter
+        </p>
+      </div>
+      <div className="flex flex-col xl:flex-row gap-6 lgl:gap-0 justify-between">
+        <div>
+          <h2 className="text-base uppercase font-titleFont mb-4">
+            Find me in
+          </h2>
+          <div className="flex gap-4">
+            <span className="bannerIcon">
+              <FaFacebookF />
+            </span>
+            <span className="bannerIcon">
+              <FaTwitter />
+            </span>
+            <span className="bannerIcon">
+              <FaLinkedinIn />
+            </span>
+          </div>
+        </div>
+        <div>
+          <h2 className="text-base uppercase font-titleFont mb-4">
+            BEST SKILL ON
+          </h2>
+          <div className="flex gap-4">
+            <span className="bannerIcon">
+              <FaReact />
+            </span>
+            <span className="bannerIcon">
+              <SiNextdotjs />
+            </span>
+            <span className="bannerIcon">
+              <SiTailwindcss />
+            </span>
+            <span className="bannerIcon">
+              <SiFigma />
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default LeftBanner
