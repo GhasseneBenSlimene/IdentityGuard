12b9781f92ad9588b991a7347007281491b3aeec
nsatge1 <nsatge@enseirb-matmeca.fr>
2024-04-06 15:12:39 +0200

    "verification with blockchain"

--- client/src/pages/Verifier/Verifier.jsx
+++ client/src/pages/Verifier/Verifier.jsx
@@ -3,6 +3,175 @@ import QRious from "qrious";
 import axios from "axios";
 import io from "socket.io-client";
 import "./style.css";
+import {Web3} from "web3";
+
+
+const contractProofABI = [
+  {
+    "inputs": [
+      {
+        "internalType": "uint256[]",
+        "name": "_a",
+        "type": "uint256[]"
+      },
+      {
+        "internalType": "uint256[][]",
+        "name": "_b",
+        "type": "uint256[][]"
+      },
+      {
+        "internalType": "uint256[]",
+        "name": "_c",
+        "type": "uint256[]"
+      },
+      {
+        "internalType": "uint256[]",
+        "name": "_inputs",
+        "type": "uint256[]"
+      }
+    ],
+    "stateMutability": "nonpayable",
+    "type": "constructor"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "name": "inputs",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256[]",
+        "name": "_a",
+        "type": "uint256[]"
+      },
+      {
+        "internalType": "uint256[][]",
+        "name": "_b",
+        "type": "uint256[][]"
+      },
+      {
+        "internalType": "uint256[]",
+        "name": "_c",
+        "type": "uint256[]"
+      }
+    ],
+    "name": "setProof",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256[]",
+        "name": "_inputs",
+        "type": "uint256[]"
+      }
+    ],
+    "name": "setInputs",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "getProof",
+    "outputs": [
+      {
+        "components": [
+          {
+            "internalType": "uint256[]",
+            "name": "a",
+            "type": "uint256[]"
+          },
+          {
+            "internalType": "uint256[][]",
+            "name": "b",
+            "type": "uint256[][]"
+          },
+          {
+            "internalType": "uint256[]",
+            "name": "c",
+            "type": "uint256[]"
+          }
+        ],
+        "internalType": "struct ProofContract.Proof",
+        "name": "",
+        "type": "tuple"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "getInputs",
+    "outputs": [
+      {
+        "internalType": "uint256[]",
+        "name": "",
+        "type": "uint256[]"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  }
+];
+
+const contractVerifABI = [
+  {
+    "inputs": [
+      {
+        "internalType": "uint256[2]",
+        "name": "_pA",
+        "type": "uint256[2]"
+      },
+      {
+        "internalType": "uint256[2][2]",
+        "name": "_pB",
+        "type": "uint256[2][2]"
+      },
+      {
+        "internalType": "uint256[2]",
+        "name": "_pC",
+        "type": "uint256[2]"
+      },
+      {
+        "internalType": "uint256[2]",
+        "name": "_pubSignals",
+        "type": "uint256[2]"
+      }
+    ],
+    "name": "verifyProof",
+    "outputs": [
+      {
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  }
+];
+
+const web3 = new Web3("http://localhost:8545");
+
 
 function Proof({ proof, onVerifyAnother }) {
   return (
@@ -37,8 +206,32 @@ function VerifierPage() {
       setSocket(newSocket);
       newSocket.emit("joinVerifier", verifierId);
 
-      newSocket.on("proof", (proofData) => {
-        setProof(proofData);
+      newSocket.on("proof", async (proofData) => {
+
+        const address_test = "0x07cb37ebafa60db9e2391d5f9d8146f5ba11f70a";
+        const contract = new web3.eth.Contract(contractProofABI, address_test);
+
+        const proof = await contract.methods.getProof().call();
+
+        const inputs = await contract.methods.getInputs().call();
+
+        const address_verif = "0xb2f6884f76f1b0cd3c056c939cdfe6df84a97fb8";
+        const contractVerif = new web3.eth.Contract(contractVerifABI, address_verif);
+
+
+
+        //const verif = await contractVerif.methods.verifyProof(proof.a, proof.b, proof.c, inputs).call();
+        //console.log(verif);
+    if(true){
+        if(inputs[0] == 1){
+            const proofData = "la verification est valide";
+            setProof(proofData);
+        }else{
+            console.log("l'age est inferieur à 18 ans");
+        }
+    } else {
+        console.log("la preuve est incorrecte");
+    }
       });
 
       return () => {
