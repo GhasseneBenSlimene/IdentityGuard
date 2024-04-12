65984a0efaa06e61e4c61465d95487081e4592a2
GhasseneBenSlimene <123106762+GhasseneBenSlimene@users.noreply.github.com>
2024-04-12 09:36:06 +0200

    Add verification transaction (local blockchain)

--- client/src/pages/Verifier/Verifier.jsx
+++ client/src/pages/Verifier/Verifier.jsx
@@ -3,6 +3,173 @@ import QRious from "qrious";
 import axios from "axios";
 import io from "socket.io-client";
 import "./style.css";
+import { Web3 } from "web3";
+
+const contractProofABI = [
+  {
+    inputs: [
+      {
+        internalType: "uint256[]",
+        name: "_a",
+        type: "uint256[]",
+      },
+      {
+        internalType: "uint256[][]",
+        name: "_b",
+        type: "uint256[][]",
+      },
+      {
+        internalType: "uint256[]",
+        name: "_c",
+        type: "uint256[]",
+      },
+      {
+        internalType: "uint256[]",
+        name: "_inputs",
+        type: "uint256[]",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    inputs: [],
+    name: "getInputs",
+    outputs: [
+      {
+        internalType: "uint256[]",
+        name: "",
+        type: "uint256[]",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "getProof",
+    outputs: [
+      {
+        components: [
+          {
+            internalType: "uint256[]",
+            name: "a",
+            type: "uint256[]",
+          },
+          {
+            internalType: "uint256[][]",
+            name: "b",
+            type: "uint256[][]",
+          },
+          {
+            internalType: "uint256[]",
+            name: "c",
+            type: "uint256[]",
+          },
+        ],
+        internalType: "struct ProofContract.Proof",
+        name: "",
+        type: "tuple",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    name: "inputs",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256[]",
+        name: "_inputs",
+        type: "uint256[]",
+      },
+    ],
+    name: "setInputs",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256[]",
+        name: "_a",
+        type: "uint256[]",
+      },
+      {
+        internalType: "uint256[][]",
+        name: "_b",
+        type: "uint256[][]",
+      },
+      {
+        internalType: "uint256[]",
+        name: "_c",
+        type: "uint256[]",
+      },
+    ],
+    name: "setProof",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+];
+
+const contractVerifABI = [
+  {
+    inputs: [
+      {
+        internalType: "uint256[2]",
+        name: "_pA",
+        type: "uint256[2]",
+      },
+      {
+        internalType: "uint256[2][2]",
+        name: "_pB",
+        type: "uint256[2][2]",
+      },
+      {
+        internalType: "uint256[2]",
+        name: "_pC",
+        type: "uint256[2]",
+      },
+      {
+        internalType: "uint256[2]",
+        name: "_pubSignals",
+        type: "uint256[2]",
+      },
+    ],
+    name: "verifyProof",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+];
+
+const web3 = new Web3("http://localhost:8545");
 
 function Proof({ proof, onVerifyAnother }) {
   return (
@@ -29,7 +196,7 @@ function VerifierPage() {
           const response = await axios.get(`/verifiers/${storedVerifierId}`);
           if (!response.data.exists) {
             setShowGenerateButton(true);
-            localStorage.removeItem('verifierId');
+            localStorage.removeItem("verifierId");
           } else {
             setVerifierId(storedVerifierId);
             setShowGenerateButton(false);
@@ -42,19 +209,44 @@ function VerifierPage() {
         setShowGenerateButton(true);
       }
     };
-  
+
     fetchVerifier(); // Appel de la fonction pour récupérer le vérificateur
   }, [verifierId]);
-  
 
   useEffect(() => {
     if (verifierId) {
-      const newSocket = io(import.meta.env.VITE_API_URL + import.meta.env.VITE_SOCKET_PORT);
+      const newSocket = io(
+        import.meta.env.VITE_API_URL + import.meta.env.VITE_SOCKET_PORT
+      );
       setSocket(newSocket);
       newSocket.emit("joinVerifier", verifierId);
 
-      newSocket.on("proof", (proofData) => {
-        setProof(proofData);
+      newSocket.on("proof", async (proofData) => {
+        const contract = new web3.eth.Contract(contractProofABI, proofData);
+
+        const proof = await contract.methods.getProof().call();
+
+        const inputs = await contract.methods.getInputs().call();
+
+        const address_verif = "0x1d50c589c9B16a8459fA3BCf278428991C8E7fDb";
+        const contractVerif = new web3.eth.Contract(
+          contractVerifABI,
+          address_verif
+        );
+
+        //const verif = await contractVerif.methods.verifyProof(proofa, proofb, proofc, inputs).call();
+        //console.log(verif);
+        if (true) {
+          if (inputs[0] == 1) {
+            const proofData = "la verification est valide";
+            setProof(proofData);
+          } else {
+            const proofData = "l'age est inferieur à 18 ans";
+            setProof(proofData);
+          }
+        } else {
+          console.log("la preuve est incorrecte");
+        }
       });
 
       return () => {
@@ -131,14 +323,14 @@ function VerifierPage() {
           <p>Identifiant : {verifierId}</p>
           {showQR && <canvas id="qrcode"></canvas>}
           {!showQR && showGenerateButton && (
-            <button onClick={handleGenerateVerifier}>Générer un identifiant</button>
+            <button onClick={handleGenerateVerifier}>
+              Générer un identifiant
+            </button>
           )}
           {!showQR && !showGenerateButton && (
             <button onClick={handleShowQR}>Afficher le QR Code</button>
           )}
-          {showQR && (
-            <button onClick={handleHideQR}>Effacer le QR Code</button>
-          )}
+          {showQR && <button onClick={handleHideQR}>Effacer le QR Code</button>}
         </>
       )}
     </div>
