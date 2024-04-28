62faa62572607f6b8559631a98b579b6ef45f324
GhasseneBenSlimene <123106762+GhasseneBenSlimene@users.noreply.github.com>
2024-04-28 19:01:48 +0200

    change axios directly to https://identityguard.me

--- client/src/config/axiosConfig.js
+++ client/src/config/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 axios.defaults.baseURL =
-  import.meta.env.VITE_API_URL + "/api" || "http://localhost:5000/api";
+  import.meta.env.VITE_API_URL + "/api" || "https://identityguard.me/api";
 
 // enable cross-origin cookies
 axios.defaults.withCredentials = true;
